bd5dbf3ac30bcaa898dda2199631889772575e7f
olbrlvalbt <olbrlvalbt@gmail.com>
2020-07-16 05:05:09 -0500

    Added glider map

--- ECA1D/Rule110Glider.h
+++ ECA1D/Rule110Glider.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <string>
 #include <regex>
 #include <map>
 
@@ -16,8 +15,31 @@ public:
 	static string GetGlider(int _multiple, string _gliderId, char _gliderPhaseChar, int _gliderPhaseNum, int _etherPhase);
 };
 
-
-
-static const map<string, std::vector<std::array<std::string, 4>>> gliderMap = {
-   {"a", GLIDER_A}
+static const map<string, vector<array<string, 4>>> GLIDERMAP = {
+   {"a", GLIDER_A},
+   {"a4", GLIDER_A4},
+   {"b", GLIDER_B},
+   {"b_", GLIDER_Bbar},
+   {"bb", GLIDER_Bbar},
+   {"bbar", GLIDER_Bbar},
+   {"b^", GLIDER_Bcirc},
+   {"bc", GLIDER_Bcirc},
+   {"bcirc", GLIDER_Bcirc},
+   {"c1", GLIDER_C1},
+   {"c2", GLIDER_C2},
+   {"c3", GLIDER_C3},
+   {"d1", GLIDER_D1},
+   {"d2", GLIDER_D2},
+   {"e", GLIDER_E},
+   {"e2", GLIDER_E2},
+   {"e3", GLIDER_E3},
+   {"e4", GLIDER_E4},
+   {"e5", GLIDER_E5},
+   {"e_", GLIDER_Ebar},
+   {"eb", GLIDER_Ebar},
+   {"ebar", GLIDER_Ebar},
+   {"f", GLIDER_F},
+   {"g", GLIDER_G},
+   {"h", GLIDER_H},
+   {"gun", GLIDER_GUN}
 };
